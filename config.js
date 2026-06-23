eca6373e352b2fe39c5dd0765b1642f241426c0b
justinkelder-del <justin.k.elder@gmail.com>
2026-06-23 18:55:18 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.LEAGUE_CONFIG = {
   sheetCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSPekMjN8yYdCq6a3HgThKAsuVahgXAOGNfCxkPsImSrdJxQo9CAh41BdSZra7ZV89G4ms78gsKuMX/pub?gid=1891216347&single=true&output=csv",
-  submitUrl: "https://script.google.com/macros/s/AKfycbyuGOrHQ56bfeVonMmVnQ_nypjrYkSnrsFRpS30xG9fk2CaSB9C4aK9ZOL9MeEUXaP9wQ/exec",
+  submitUrl: "https://script.google.com/macros/s/AKfycbyuGOrHQ56bfeVonMmVnQ_nypjrYkSnrsFRpS3OxG9fk2CaSB9C4aK9Z0L9MeEUXaP9wQ/exec",
   players: ["Jake", "Justin", "DiBs"],
   points: { 1: 5, 2: 3, 3: 1 }
 };
